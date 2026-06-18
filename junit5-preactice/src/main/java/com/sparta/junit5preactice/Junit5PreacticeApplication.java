a55c6b03c25527ce8ca9afc4fc1e156c9e2c8b17
Lim JinKeon <0907john24@gmail.com>
2026-06-18 17:08:32 +0900

    jpa&junit5

---
+++ junit5-preactice/src/main/java/com/sparta/junit5preactice/Junit5PreacticeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.junit5preactice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5PreacticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Junit5PreacticeApplication.class, args);
+    }
+
+}
