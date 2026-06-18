1846bb1ec314fa3d3d04668678a4e403f5805b4e
Lim JinKeon <0907john24@gmail.com>
2026-06-18 17:20:21 +0900

    위치 변경

---
+++ sparta_msa/com.spring-cloud.resilience4j.sample/src/main/java/com/spring_cloud/resilience4j/sample/products/ProductService.java
@@ -0,0 +1,24 @@
+package com.spring_cloud.resilience4j.sample.products;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackGetProductsDetails")
+    public Product getProductDetails(String productId) {
+
+        if ("111".equals(productId)) {
+            throw new RuntimeException("Empty Response body");
+        }
+
+        return new Product(productId, "Sample Product");
+    }
+
+    public Product fallbackGetProductsDetails(String productId, Throwable t) {
+        return new Product(productId, "Fallback Product");
+    }
+}
